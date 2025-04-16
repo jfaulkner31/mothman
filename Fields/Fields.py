8d428ed11ef651841d53317c97b10cb257c69745
Jonathon <jfaulkner31@gatech.edu>
2025-04-16 15:55:05 -0400

    latest

--- Fields/Fields.py
+++ Fields/Fields.py
@@ -12,29 +12,153 @@ class Field:
 # General scalar field class (a field with a single scalar variable such as Temperature)
 class ScalarField(Field):
   """
-  Scalar variable for transport model - represented by a T
+  Cell centered field at the element centroid - represented by .T
   """
   def __init__(self, name: str, initial_value: (np.ndarray, float), mesh: Mesh_1D):
+    #################################################
     # super from Field class
     super().__init__(initial_value=initial_value, name=name, mesh=mesh,)
-
     # Now to make initial values for this field based on if array or float
     if isinstance(initial_value, np.ndarray):
       if len(initial_value) == self.mesh.nz:
         self.initial_value = initial_value
       else:
         raise Exception("nz on variable input mesh must be the same as the length of the initial condition vector!")
-
     elif isinstance(initial_value, float):
       self.initial_value = np.ones(self.mesh.nz)*initial_value
-
     else:
       raise Exception("Initial value must be of np.ndarray or float.")
+    #################################################
 
     # Assigns T as initial values based on deepcopy
     self.T = copy.deepcopy(self.initial_value)
 
-    # More stuff..
+    # Gradient stuff:
+
+    # interpolate_to_faces() -> uses Equation 9.5 to compute field values at faces instead of ecntroid
+    #   g_C * T_C + (1-g_C)*T_F = T_f
+    #   TODO make sure BC returns correct get_face_value
+
+    # get_only_face_gradient -> uses 9.33 in 1D to compute face gradient
+    #   TODO - make sure BC returns correct gradient method
+
+    #   get_gradient_at_centroid() -> Computes gradient at centroid based on face values using gauss green
+    #   1. get face values
+    #   2. do gauss green gradient to get grad_C
+
+    # get_gradient_at_faces() -> Computes gradient at faces.
+
+  def interpolate_to_faces(self, upper_bc, lower_bc):
+    """
+    - interpolates field from centroid to faces using Equation 9.5:
+      phi_f = g_C phi_C + (1-g_C)phi_F
+    - This is second order accurate as long as the centroid lies on the same vector as the face centroid
+    - In 1D this is always second order accurate.
+    - for face values we use the values from the associated boundary condition
+    """
+    vals = np.array([])
+    for idx, cid in enumerate(self.mesh.cidList):
+      this_T_value = self.T[idx]
+      try:
+        upper_neighbor_T_value = self.T[idx+1]
+        g = self.mesh.cells[cid].geoUpper
+        upperFaceValue = g * this_T_value + (1-g) * upper_neighbor_T_value
+        vals = np.append(vals,upperFaceValue)
+      except:
+        ### if this hits then it means there is no upper neighbor - e.g. last cell so the upper face
+        ### value is now an outer cell - so we do nothing.
+        pass
+
+    # now handle bc's - these must have a method called get_face_value.
+    lowest_face_value = lower_bc.get_face_value()
+    highest_face_value = upper_bc.get_face_value()
+    vals = np.append(lowest_face_value, vals)
+    vals = np.append(vals, highest_face_value)
+    return vals ### returns field interpolated to the faces using interpolation + bc's
+
+  def get_gradient_at_centroid(self, upper_bc, lower_bc):
+    face_values = self.interpolate_to_faces(upper_bc=upper_bc, lower_bc=lower_bc)
+    grad_C_vec = np.array([])
+    for idx, cid in enumerate(self.mesh.cidList):
+      this_cell = self.mesh.cells[cid]
+      lower_area = this_cell.lowerArea
+      upper_area = this_cell.upperArea
+      vol = this_cell.dz * (lower_area + upper_area) / 2.0
+      upper_phi_f = face_values[idx+1]
+      lower_phi_f = face_values[idx]
+      grad_C = ( lower_phi_f * lower_area + upper_area * upper_phi_f ) / vol
+      grad_C_vec = np.append(grad_C_vec, grad_C)
+    return grad_C_vec # returns gradient at every single cell centroid
+
+  def get_gradient_at_faces(self, upper_bc, lower_bc):
+    """
+      First we get barred gradient from 9.34 for inner faces.
+      Then we get non-barred gradient using equation 9.33.
+      For upper and lower boundary we can get the gradient by passing in the BC.
+    """
+    grad_C_vec = self.get_gradient_at_centroid(upper_bc=upper_bc, lower_bc=lower_bc) # gets gradient at centroids of cells for this variable
+    grad_f_bar_vec = np.array([]) # barred gradient
+    grad_f_vec = np.array([]) # real gradient
+    # Get grad_f_bar at all the INNER faces.
+    # then get grad_f at all the INNER faces.
+    for idx, cid in enumerate(self.mesh.cidList):
+      grad_C = grad_C_vec[idx]
+      try:
+        this_cell = self.mesh.cells[cid]
+        upper_neighbor_cell = self.mesh.cells[cid+1]
+        gC = this_cell.geoUpper
+        gF = upper_neighbor_cell.geoLower
+        grad_F = grad_C_vec[idx+1]
+
+        # grad_f_bar
+        grad_f_bar = gC * grad_C + gF*grad_F
+        grad_f_bar_vec = np.append(grad_f_bar_vec, grad_f_bar)
+
+        # grad_f
+        phi_C = self.T[idx]
+        phi_F = self.T[idx]
+        dCF = this_cell.upperNeighborDist
+        grad_f = (phi_F - phi_C) / dCF # equation 9.33 ---> uses e = unit vector = 1.0
+        grad_f_vec = np.append(grad_f_vec, grad_f)
+
+      except:
+        # last cell implies no upper neighbor
+        pass
+
+    # now get lower and upper face gradient based on boundary conditions.
+    upper_real_face_gradient = upper_bc.get_face_gradient()
+    lower_real_face_gradient = lower_bc.get_face_gradient()
+    grad_f_vec = np.append(lower_real_face_gradient, grad_f_vec)
+    grad_f_vec = np.append(grad_f_vec, upper_real_face_gradient)
+    return grad_f_vec ## returns gradient at ALL faces in the problem
+
+  def get_only_face_gradient(self, upper_bc, lower_bc):
+    """
+      Returns gradient at faces for the 1D problem using equation 9.33.
+      For upper and lower boundary we can get the gradient by passing in the BC.
+    """
+    grad_f_vec = np.array([]) # real gradient
+    # Get grad_f_bar at all the INNER faces.
+    # then get grad_f at all the INNER faces.
+    for idx, cid in enumerate(self.mesh.cidList):
+      try:
+        this_cell = self.mesh.cells[cid]
+        # grad_f
+        phi_C = self.T[idx]
+        phi_F = self.T[idx+1]
+        dCF = this_cell.upperNeighborDist
+        grad_f = (phi_F - phi_C) / dCF # equation 9.33 ---> uses e = unit vector = 1.0
+        grad_f_vec = np.append(grad_f_vec, grad_f)
+
+      except:
+        # last cell implies no upper neighbor -- we get exception when idx+1 runss
+        pass
+
+    # now get lower and upper face gradient based on boundary conditions.
+    upper_real_face_gradient = upper_bc.get_face_gradient()
+    lower_real_face_gradient = lower_bc.get_face_gradient()
+    grad_f_vec = np.append(lower_real_face_gradient, grad_f_vec)
+    grad_f_vec = np.append(grad_f_vec, upper_real_face_gradient)
 
 class FaceField:
   """
--- Meshing/Meshing.py
+++ Meshing/Meshing.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Cell:
-  def __init__(self, dz: float, upperZ: float, lowerZ: float, upperType: str, lowerType: str, upperArea: float, lowerArea: float, cid: int, upperFid: int, lowerFid: int):
+  def __init__(self, dz: float, upperZ: float, lowerZ: float, upperType: str, lowerType: str, upperArea: float, lowerArea: float, cid: int):
     self.cid = cid # cell id
     self.upperZ = upperZ # upper face position
     self.lowerZ = lowerZ # lower face position
@@ -20,20 +20,28 @@ class Cell:
     self.lowerNeighborCid = None # stays at None if no lower neighbor - otherwise is lower neighbor cell id
     self.upperNeighborCid = None
 
+
+    # d_CF vector --- = scalar value in 1D
     self.lowerNeighborDist = None # stays at None if no dist - otherwise is lower neighbor distance to centroid
     self.upperNeightborDist = None
 
+    # geoemtric diffusion coefficients
     self.upperNeighborGdiff = None
     self.lowerNeighborGdiff = None
 
-    self.upperFid=upperFid
-    self.lowerFid=lowerFid
+    # geometric weighting factors
+    self.geoUpper = None # geometric weighting factors --- equation 9.6
+    self.geoLower = None
+
+    # e_CF unit vector --- 1.0 in 1D
+    self.ecfUpper = 1.0 # always 1 for 1D - defined in equation 9.34 --- e.g. just a unit vector
+    self.ecfLower = 1.0
 
 class Mesh_1D:
   """
   Creates a one dimensional mesh object.
   """
-  def __init__(self, nodeCoords: np.ndarray, faceAreas: np.ndarray):
+  def __init__(self, nodeCoords: np.ndarray, faceAreas: list, doStaggered=True):
 
     # proofreading / exceptions
     self.nodeCoords = np.unique(np.sort(nodeCoords)) # nodeCoords = locations of zFaces starting at boundary
@@ -49,7 +57,6 @@ class Mesh_1D:
 
     # now assign cells and stuff
     cid = int(0)
-    fid = int(0)
     for idx, node in enumerate(self.nodeCoords):
       if idx == 0:
         continue
@@ -69,15 +76,14 @@ class Mesh_1D:
                                upperType=upperType,
                                lowerType=lowerType,
                                upperArea=self.faceAreas[idx],
-                               lowerArea=self.faceAreas[idx-1],
-                               upperFid=fid,
-                               lowerFid=fid-1)
+                               lowerArea=self.faceAreas[idx-1])
         self.cidList.append(cid)
-        self.fidList.append(fid)
 
 
-        fid += 1 # progress fid
         cid += 1 # progress cid
+
+    self.cidLast = cid - 1
+    self.cidFirst = 0
 
     # now we iterate across all cells and define distance to neighbors and neighbor ids
     # ids and distances will be None if the neighbor does not exist
@@ -85,17 +91,38 @@ class Mesh_1D:
       this_cell = self.cells[cid]
       # lower cell try
       try:
-        this_cell.lowerNeighborCid = self.cells[this_cell.cid - 1].cid
+        # lower neighbor cid
+        lwr_n_cid = self.cells[this_cell.cid - 1].cid # lower neighbor cid
+        this_cell.lowerNeighborCid = lwr_n_cid
+
+
+        # absolute distance to neighbor - d_CF
         this_cell.lowerNeighborDist = abs(this_cell.centroid - self.cells[this_cell.cid - 1].centroid)
+
+        # Geometric diffusion coefficientss
         this_cell.lowerNeighborGdiff = this_cell.lowerArea / this_cell.lowerNeighborDist
+
+        # geometric weighting factor for lower
+        this_cell.geoLower = abs((self.cells[lwr_n_cid].dz/2) / this_cell.lowerNeighborDist)
+
       except:
         pass
 
       # upper cell try
       try:
-        this_cell.upperNeighborCid = self.cells[this_cell.cid + 1].cid
+        # lower neighbor cid
+        upr_n_cid = self.cells[this_cell.cid + 1].cid # lower neighbor cid
+        this_cell.upperNeighborCid = upr_n_cid
+
+        # absolute distance to neighbor - d_CF
         this_cell.upperNeighborDist = abs(this_cell.centroid - self.cells[this_cell.cid + 1].centroid)
+
+        # Geometric diffusion coefficientss
         this_cell.upperNeighborGdiff = this_cell.upperArea/this_cell.upperNeighborDist
+
+        # geometric weighting factor for lower
+        this_cell.geoUpper = abs((self.cells[upr_n_cid].dz/2) / this_cell.upperNeighborDist)
+
       except:
         pass
 
@@ -103,6 +130,50 @@ class Mesh_1D:
     for cid in self.cells.keys():
       self.centroids.append(self.cells[cid].centroid)
 
+    # setup for the staggered mesh variables.
+    if doStaggered:
+      self.setup_staggered_mesh()
 
 
+  def setup_staggered_mesh(self):
+    """
+    Takes in a Mesh_1D and outputs a corresponding staggered mesh.
+    Output mesh:
+      Node coords should the be original mesh's faces
+      faceAreas should be averaged between faces.
+
+    Original Mesh:
+    |   .   |   .   |   .   |   .   |    .    |
+
+    New Mesh:
+    |.  |   .   |   .   |   .   |   .    |    .|
+    """
+
+    # setup for node coords for the staggered mesh ( faces )
+    dx0 = self.cells[self.cidFirst].dz / 2
+    dxLast = self.cells[self.cidLast].dz / 2
+    centroid0 = self.nodeCoords[0]
+    centroidLast = self.nodeCoords[-1]
+    face0 = centroid0 - dx0
+    faceLast = centroidLast + dxLast
+
+    firstPart = np.append([face0], self.centroids)
+    secondPart = [faceLast]
+    self.stagNodeCoords = np.append(firstPart, secondPart)
+
+    # setup the face areas for the staggered mesh.
+    stagFaceAreas = [self.faceAreas[0]]
+    stagFaceAreas = np.append([stagFaceAreas[0]], np.array(self.faceAreas[0:-1])/2 + np.array(self.faceAreas[1::])/2)
+    stagFaceAreas = np.append(stagFaceAreas, [stagFaceAreas[-1]])
+
+    # make staggered mesh now:
+    self.stagMesh = Mesh_1D(nodeCoords=self.stagNodeCoords, faceAreas=stagFaceAreas, doStaggered=False)
+
+    # now make a dict in the original mesh where you put in CID and east/west face
+    # and you get the CID in the staggered mesh associated with that face.
+    self.stagCellEast = {} # pass in a CID on the main mesh and return the CID from the staggered mesh that overlaps east face
+    self.stagCellWest = {}
+    for cid in self.cells.keys():
+      self.stagCellWest[cid] = cid
+      self.stagCellEast[cid] = cid + 1
 
